5d391e311969d90a08046963e0f7287bb4b19291
LSU-blara4 <60485940+LSU-blara4@users.noreply.github.com>
2022-04-20 17:41:26 -0500

    Update Convertor.java

--- project3csc4102/Convertor.java
+++ project3csc4102/Convertor.java
@@ -2,5 +2,13 @@ package project3csc4102;
 import java.util.Stack;
 
 public class Convertor {
+
+    public static String convertToPostfix(String infixExpr3) {
+        return null;
+    }
+
+    public static Double evaluatePostfix(String postfixExpr1) {
+        return null;
+    }
     
 }
